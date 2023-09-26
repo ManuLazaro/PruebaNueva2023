8584441e71a74a498fb8da57c020ef305c974f5b
Manu <manulazaro2000@gmail.com>
2023-09-26 09:21:32 +0200

    tercer commit que hago

---
+++ src/Empleado.java
@@ -0,0 +1,17 @@
+public class Empleado {
+
+    int edad;
+
+    public Empleado(int edad) {
+        this.edad = edad;
+    }
+
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
